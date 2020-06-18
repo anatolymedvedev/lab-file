1b1385bbe358af5c81cfdc20bf7db369968ce2bc
anatolymedvedev <66411230+anatolymedvedev@users.noreply.github.com>
2020-06-18 15:35:32 +0700

    Create file.c

---
+++ file.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])	
+{		 
+	FILE *result = fopen("result.txt", "w");
+  for(int i = 1; i < argc; i++)
+	{
+		FILE *task_file = fopen(argv[i], "r");
+		char c = fgetc(task_file);
+		while(c != EOF)
+		{	
+			printf("%c", c);
+			fputc(c, result);
+			c = fgetc(task_file);
+		}
+		printf("\n");
+		fputc('\n', result);
+		fclose(task_file);
+	}
+
+	fclose(result);
+
+	return 0;
+}
